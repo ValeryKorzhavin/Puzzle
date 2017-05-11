709bb7d1e3d0ff8fc11af65d91e52106289db2b8
Valery Korzhavin <korzhavin@uchi.ru>
2017-05-11 12:10:05 +0300

    Added shuffling of fragments

--- app/_include/js/script.js
+++ app/_include/js/script.js
@@ -1,4 +1,35 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomPos() {
+  return {
+    top: getRandomInt(10, 271),
+    left: getRandomInt(460, 751),
+  }
+}
+
+function shuffleFragments(fragments) {
+  fragments.each(function(index, element) {
+    var pos = getRandomPos();
+    $(element).css({
+      'top': pos.top,
+      'left': pos.left,
+    });
+  });
+}
+
 $(document).ready(function() {
   // При загрузке страницы выполнится весь написанный здесь код
-  // alert("Привет!!!");
+
+  var data = {
+    button: $('.check'),
+    puzzles: $('.puzzle'),
+    images: $('.image'),
+    parent: $('.workspace'),
+    scene: $('.scene'),
+  };
+
+  shuffleFragments(data.images);
+
 });
